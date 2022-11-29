a6b5b75bcbe2cfba0128a606388fa3898cbda342
ahmetercinural <rJCKz1KfM9ZiXy4n>
2022-11-29 13:33:40 +0300

    testfolder dosyası projeye eklendi..

---
+++ just_git_learn/lib/testFolder/test.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class TestView extends StatefulWidget {
+  const TestView({super.key});
+
+  @override
+  State<TestView> createState() => _TestViewState();
+}
+class _TestViewState extends State<TestView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
